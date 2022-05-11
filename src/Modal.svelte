fc422f74ec3d165127d2b7e7bca0a71f59f37adb
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-05-10 23:26:07 -0400

    Modal component template

---
+++ src/Modal.svelte
@@ -0,0 +1,17 @@
+<!-- Modal with svelma -->
+<!-- https://c0bra.github.io/svelma/install/ -->
+<script>
+import { faPenAlt } from '@fortawesome/free-solid-svg-icons';
+
+  import { Button, ModalCard, Field, Input } from 'svelma';
+  import Fa from 'svelte-fa';
+
+  let active = false;
+</script>
+
+<Button class="block" on:click={() => active = !active}><Fa icon={faPenAlt}/></Button>
+<ModalCard bind:active={active} title="My Modal Title">
+  <Field label="Textarea">
+    <Input type="textarea" maxlength="200" />
+  </Field>
+</ModalCard>
