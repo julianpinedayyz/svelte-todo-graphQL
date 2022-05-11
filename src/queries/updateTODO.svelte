40dec4ef603456067d976cbb2169e34e9e24b928
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-05-10 23:29:54 -0400

    UpdateTODO mutation

---
+++ src/queries/updateTODO.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import { gql } from "@apollo/client";
+
+  export const updateTodoMutation = gql`
+    mutation ($todoID: Int!, $todoStatus: Boolean) {
+      updateTodoById(input: {id: $todoID, todoPatch: {done: $todoStatus}}) {
+        clientMutationId
+        todo {
+          done
+          id
+        }
+      }
+    }
+  `;
+</script>
+
+
+
