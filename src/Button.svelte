4ea85af238f5ca256074e53e63e21b01c94cdd52
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-05-13 23:04:18 -0400

    Created Button component first steps

---
+++ src/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Icon from "./Icon.svelte"
+
+</script>
+
+<!-- <button
+  {...props}
+  type={nativeType}
+  class:is-inverted={inverted}
+  class:is-loading={loading}
+  class:is-outlined={outlined}
+  class:is-rounded={rounded}
+  on:click>
+  {#if iconLeft}
+    <Icon pack={iconPack} icon={iconLeft} size={iconSize} />
+  {/if}
+  <span>
+    <slot />
+  </span>
+  {#if iconRight}
+    <Icon pack={iconPack} icon={iconRight} size={iconSize} />
+  {/if}
+</button> -->
+
+<Icon/>
