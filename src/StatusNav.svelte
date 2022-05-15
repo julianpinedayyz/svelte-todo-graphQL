2a5574d4d80a85e09deebec0e89e2b8fd8246ae0
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-05-14 23:43:57 -0400

    Making StatusNav component

---
+++ src/StatusNav.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { todos } from './queries/getTODOS.svelte';
+</script>
+
+<nav class="level">
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Total Posts</p>
+      <p class="title">{$todos.data.todosList.length}</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Completed</p>
+      <p class="title">
+        {$todos.data.todosList.filter((todo) => todo.done).length}
+      </p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Followers</p>
+      <p class="title">456K</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Likes</p>
+      <p class="title">789</p>
+    </div>
+  </div>
+</nav>
