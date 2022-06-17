f045481d6479bfa9ad02dfe103ca6aa7a74e1216
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-06-16 22:24:19 -0400

    More settings on apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   client: {
+    tagName: "graphql",
     service: {
       name: "svelte-todo-graphQL",
       url: "http://localhost:5055/graphql",
       localSchemaFile: "./src/schema/schema.graphql",
     },
+    includes: ["./src/**/*.js"],
+    excludes: ["**/__tests__/**"],
   },
 };
